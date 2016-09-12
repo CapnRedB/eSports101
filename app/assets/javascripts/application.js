db6b7740ca205fc94a6ade8ffff6ef61240fcdea
Aaron Levinson <aaron.levinson1000@gmail.com>
2016-09-12 10:07:01 -0500

    bottom row node works and more elegant code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,7 @@
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts	,
 // or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
@@ -16,29 +16,34 @@
 //= require_tree .
 var toolTip = false;
 $( document ).ready( function( e ) {
-	$( '.gameNode1' ).on( 'click', function( e ) {
-
-		var newDiv = $( event.target ).clone();
-		newDiv.addClass( 'toolTip' );
-		newDiv.attr( 'id', 'toolTip' );
-		$( '.gameDeck' ).append( newDiv );
-		var fillSpace = $( '#t1Top' );
-		newDiv.css( 'background-color', 'green' );
-		newDiv.css( 'position', 'absolute' );
-		var position = $( event.target ).position();
-		//console.log( position );
-		newDiv.css( 'left', position.left );
-		newDiv.css( 'top', position.top );
-		newDiv.css( 'z-index', 1 );
-		newDiv.animate( {
-			left: fillSpace.position().left + "px",
-			height: '200px',
-			width: '1024px'
-		}, 1000 );
+	$( '.gameDeck' ).on( 'click', function( e ) {
+		if ( !toolTip ) {
+			toolTip = true;
+			console.log( "expand" );
+			var newDiv = $( event.target ).clone();
+			newDiv.addClass( 'toolTip' );
+			newDiv.attr( 'id', 'toolTip' );
+			$( '.gameDeck' ).append( newDiv );
+			var fillSpace = $( '#t1Top' );
+			newDiv.css( 'background-color', 'green' );
+			newDiv.css( 'position', 'absolute' );
+			var position = $( event.target ).position();
+			//console.log( position );
+			newDiv.css( 'left', position.left );
+			newDiv.css( 'top', position.top );
+			newDiv.css( 'z-index', 1 );
+			newDiv.animate( {
+				left: fillSpace.position().left + "px",
+				top: fillSpace.position().top + "px",
+				height: '200px',
+				width: '1024px'
+			}, 1000 );
+		}
 	} );
 
 	$( '.gameDeck' ).mouseleave( function() {
 		$( '#toolTip' ).remove();
+		toolTip = false;
 	} )
 
 } )
